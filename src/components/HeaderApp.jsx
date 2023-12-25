bfd0fc1839e7283d227621545397b196566538d5
miloter <miloter@outlook.es>
2023-12-25 23:54:27 +0100

    No fue mucho trabajo, pero fue honesto

--- src/components/HeaderApp.jsx
+++ src/components/HeaderApp.jsx
@@ -1,7 +1,7 @@
 export default function HeaderApp() {
     return (
         <h1 className="fw-bold display-5 text-center">
-            Seguimiento Pacientes
+            Seguimiento Pacientes {' '} con GitHub {' '}
             <span className="text-primary d-block">Veterinaria</span></h1>
     );
 }
